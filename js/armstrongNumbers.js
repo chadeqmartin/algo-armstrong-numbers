ac688ad9311357ce89382542e563e09474e3e8d1
chadeqmartin <chadeqmartin@gmail.com>
2023-10-06 23:00:43 -0400

    updated with working solution

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,16 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(arrOfNums) {
+    return arrOfNums.filter((elem) => {
+        let elemToCheck = elem;
+        let sum = 0;
+        let lengthOfNumToCheck = elemToCheck.toString().length;
 
+        while(elemToCheck > 0){
+           let eachDigit = elemToCheck % 10;
+           sum += eachDigit ** lengthOfNumToCheck
+           elemToCheck = Math.floor(elemToCheck/10)
+        }
+      return elem === sum
+    })
 };
